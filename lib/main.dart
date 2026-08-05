f36d049d826ca853854ac56bcdbfbd4399ddb09d
UmmeHaniBaig <107580523+UmmeHaniBaig@users.noreply.github.com>
2026-08-05 18:30:01 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,15 @@
 import 'package:flutter/material.dart';
-import 'profile_card.dart';
-import 'counter.dart';
+import 'login_screen.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const FoodApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+class FoodApp extends StatelessWidget {
+  const FoodApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Hani Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color.fromARGB(255, 11, 74, 56),
-        ),
-      ),
-      home: const MyHomePage(title: 'Flutter Hani Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ProfileCard(name: 'Your Name', bio: 'This is my bio.'),
-            SizedBox(height: 30),
-            Counter(),
-          ],
-        ),
-      ),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
     );
   }
 }
