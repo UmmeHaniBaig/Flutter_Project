f4abc5c381f5b056f962508822869a189b5f5658
UmmeHaniBaig <107580523+UmmeHaniBaig@users.noreply.github.com>
2026-08-05 18:39:14 +0500

    Add Product class with attributes and constructor

--- lib/model/product.dart
+++ lib/model/product.dart
@@ -1,1 +1,21 @@
+class Product {
+  final String id;
+  final String name;
+  final String category;
+  final double price;
+  final double rating;
+  final String imageUrl;
+  final String description;
+  final bool isHot;
 
+  const Product({
+    required this.id,
+    required this.name,
+    required this.category,
+    required this.price,
+    required this.rating,
+    required this.imageUrl,
+    required this.description,
+    this.isHot = false,
+  });
+}
