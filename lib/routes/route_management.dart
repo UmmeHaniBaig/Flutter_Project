1954b5607d6434236f449dd38e3365bbf54528bb
UmmeHaniBaig <107580523+UmmeHaniBaig@users.noreply.github.com>
2026-08-05 18:41:38 +0500

    Implement route management for navigation

--- lib/routes/route_management.dart
+++ lib/routes/route_management.dart
@@ -1,1 +1,39 @@
+import 'package:flutter/material.dart';
+import 'app_routes.dart';
+import '../login_screen.dart';
+import '../signup_screen.dart';
+import '../home_screen.dart';
+import '../UserProfile_card.dart';
+import '../cart_screen.dart';
+import '../detail_screen.dart';
 
+class RouteManagement {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case AppRoutes.login:
+        return MaterialPageRoute(builder: (_) => const LoginScreen());
+
+      case AppRoutes.signup:
+        return MaterialPageRoute(builder: (_) => const SignupScreen());
+
+      case AppRoutes.home:
+        return MaterialPageRoute(builder: (_) => const HomeScreen());
+
+      case AppRoutes.profile:
+        return MaterialPageRoute(builder: (_) => const UserProfileCard());
+
+      case AppRoutes.cart:
+        return MaterialPageRoute(builder: (_) => const CartScreen());
+
+      case AppRoutes.detail:
+        final food = settings.arguments as Map<String, dynamic>;
+        return MaterialPageRoute(builder: (_) => DetailScreen(food: food));
+
+      default:
+        return MaterialPageRoute(
+          builder: (_) =>
+              const Scaffold(body: Center(child: Text("Page not found"))),
+        );
+    }
+  }
+}
