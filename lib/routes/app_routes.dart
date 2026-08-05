c51280d208cddecfb75d04da1fd6ab8d0b34aa77
UmmeHaniBaig <107580523+UmmeHaniBaig@users.noreply.github.com>
2026-08-05 18:40:32 +0500

    Add AppRoutes class with route constants

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,1 +1,8 @@
-
+class AppRoutes {
+  static const String login = '/login';
+  static const String signup = '/signup';
+  static const String home = '/home';
+  static const String profile = '/profile';
+  static const String cart = '/cart';
+  static const String detail = '/detail';
+}
